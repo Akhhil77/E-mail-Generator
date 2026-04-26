a24b6aa4396f87a7899e6f4f57e8afe0b5a82961
Akhil <akhiljoseph11111@gmail.com>
2026-04-26 10:52:43 +0530

    app.jsx path issue

--- Email-generator-frontend/src/App.jsx
+++ Email-generator-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Home from '/src/components/Home.jsx'
+import Home from './Components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
